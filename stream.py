677414b184d858c1dd40974f2e55f3027cad175c
Mike <michaelerickstad@gmail.com>
2017-04-27 22:40:06 -0700

    correct an error in RX_buffer load tracking

--- stream.py
+++ stream.py
@@ -16,12 +16,12 @@ from lib.util import deltaTime
 RX_BUFFER_SIZE = 128
 
 # Initialize the args
-args = argv.arg(description='Simple python GRBL streamer', 
+args = argv.arg(description='Simple python GRBL streamer',
                 getFile=True)
 
 # Let go!
 start = datetime.now()
-puts(colored.blue(' Starting file: %s \n at %s'%(args.gcode.name, start)))
+puts(colored.blue(' Steaming file: %s \n at %s'%(args.gcode.name, start)))
 
 # read the full file and then close so that it cannot change.
 # I am pretty sure even at 1M lines, I should be able to hold this in mem
@@ -35,20 +35,20 @@ with Communicate(args.device, args.speed, timeout=args.timeout,
   inBuf = [] # array of length of lines in buffer
   for i,line in enumerate(progress.bar(lines)):
     # Strip comments/spaces/new line, capitalize, and add line ending
-    l = re.sub('\s|\(.*?\)','',line.strip()).upper()+'\n'  
+    l = re.sub('\s|\(.*?\)','',line.strip()).upper()+'\n'
 
     # if 'M' in l:
     #   continue
 
-    
+
     # if this was a comment or blank line just go to the next one
     if len(l.strip()) == 0:
       continue
-    
+
     out = ''
     try:
         # if the serial is has text and we have not filled the buffer
-        while sum(inBuf) >= RX_BUFFER_SIZE-1 | serial.inWaiting():
+        while sum(inBuf) >= RX_BUFFER_SIZE-len(l)-1 | serial.inWaiting():
           tmp = serial.readline().strip()
 
           # allows you to debug without a serial connection
@@ -72,19 +72,19 @@ with Communicate(args.device, args.speed, timeout=args.timeout,
             serial.run(x)
             if '~' in x: break
     #  send the command
-    serial.write(l)
     inBuf.append(len(l))
-    
+    serial.write(l)
+
     if not args.quiet:
       puts(colored.blue('[%04d][Sent: %s][Buf:%3d]'%(i,l.strip().rjust(30),sum(inBuf))) +
            colored.green(' Rec: %s'%(out))+' '*12)
-      
+
   # It seems everything is ok, but dont reset everything untill buffer completes
   puts(
     colored.green('''
   gCode finished streaming!
       Finished at: %s
-      RunTime: %s'''%(datetime.now(), deltaTime(start))) + 
+      RunTime: %s'''%(datetime.now(), deltaTime(start))) +
     colored.red('''
   !!! WARNING: Please make sure that the buffer clears before finishing...''') )
   try:
@@ -97,4 +97,3 @@ with Communicate(args.device, args.speed, timeout=args.timeout,
           x = raw_input('GRBL> ').strip()
           serial.run(x)
           if '~' in x: break
-      
